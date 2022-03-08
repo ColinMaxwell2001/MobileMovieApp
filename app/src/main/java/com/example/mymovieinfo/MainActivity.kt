ced38ce86fdef5c83fb0af4d6d689670c4b5e67e
ColinMaxwell2001 <95194573+ColinMaxwell2001@users.noreply.github.com>
2022-03-08 18:20:19 -0500

    GSON and SerializedName
    Added GSON dependencies and SerializedName into DTO

--- app/src/main/java/com/example/mymovieinfo/MainActivity.kt
+++ app/src/main/java/com/example/mymovieinfo/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.mymovieinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
+
+
 }
