101bf158f4663325b33914c07563e4ecc0095c7c
turnbopd <turnbopd@mail.uc.edu>
2022-03-08 21:56:34 -0500

    My commit

--- app/src/main/java/com/example/mymovieinfo/MainActivity.kt
+++ app/src/main/java/com/example/mymovieinfo/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.mymovieinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+<<<<<<< Updated upstream
+=======
+import org.w3c.dom.Text
+import java.io.IOException
+import java.io.InputStream
+>>>>>>> Stashed changes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,4 +15,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     //test 2
     }
+<<<<<<< Updated upstream
+=======
+    fun Greeting(name: String){
+        Text(text = "Welcome $name!")
+    }
+
+>>>>>>> Stashed changes
 }
---
+++ app/src/main/java/com/example/mymovieinfo/ui/theme/Color.kt
@@ -0,0 +1,5 @@
+package com.example.mymovieinfo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Silver100 = Color(0xFFF0FFFF)
