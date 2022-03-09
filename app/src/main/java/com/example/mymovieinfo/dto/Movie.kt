3b39be85dbcd9e6487181b8f9fb54a3202f21ff9
schottlw <schottlw@mail.uc.edu>
2022-03-09 11:32:35 -0500

    Restructured dto according to Kotlin Style Guides

--- app/src/main/java/com/example/mymovieinfo/dto/Movie.kt
+++ app/src/main/java/com/example/mymovieinfo/dto/Movie.kt
@@ -2,9 +2,12 @@ package com.example.mymovieinfo.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Movie(@SerializedName("rank")var rank: Int, @SerializedName("title")var title: String,
-                 @SerializedName("country") var country: String, @SerializedName("boxOfficeGross")var boxOfficeGross: Double,
-                 @SerializedName("openingWeekendGross")var openingWeekendGross: Double, @SerializedName("distributor")var distributor: String){
+data class Movie(@SerializedName("rank")var rank: Int,
+                 @SerializedName("title")var title: String,
+                 @SerializedName("country") var country: String,
+                 @SerializedName("boxOfficeGross")var boxOfficeGross: Double,
+                 @SerializedName("openingWeekendGross")var openingWeekendGross: Double,
+                 @SerializedName("distributor")var distributor: String) {
 
     override fun toString(): String {
         return title
