e2e72a08f4f43af5e3c7b6fd3560627fdb8f672a
JMaz-15 <65188299+JMaz-15@users.noreply.github.com>
2022-03-09 17:30:57 -0500

    Created movie name text field for UI - Jetpack Compose is not integrated with this project so I am unable to make the necessary imports, however the framework should be in place.

--- app/src/main/java/com/example/mymovieinfo/MainActivity.kt
+++ app/src/main/java/com/example/mymovieinfo/MainActivity.kt
@@ -9,7 +9,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContent{
+            FindMovie()
+        }
     }
-
-
 }
+
+@Composable
+fun FindMovie(){
+    var movieName by remember { mutableStateOf("")}
+    OutlinedTextField(value = movieName,
+    onValueChange = {movieName = it},
+    label = {Text(context.getString(R.string.movieName))})
+}
+
+
+
+
