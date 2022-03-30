bb952b7556d4a5cb77d5ffdd38af238c8dc961d3
YifeiYang20 <yang3yf@mail.uc.edu>
2022-03-29 20:54:29 -0400

    3.29.4（temporary）

--- app/src/main/java/com/example/mymovieinfo/dto/Movie.kt
+++ app/src/main/java/com/example/mymovieinfo/dto/Movie.kt
@@ -1,6 +1,8 @@
 package com.example.mymovieinfo.dto
 
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 
 data class Movie(@SerializedName("rank")var rank: Int, @SerializedName("title")var title: String,
                  @SerializedName("country") var country: String, @SerializedName("boxOfficeGross")var boxOfficeGross: Double,
@@ -10,7 +12,23 @@ data class Movie(@SerializedName("rank")var rank: Int, @SerializedName("title")v
         return title
     }
 
+    val gson = Gson()
 
+    //convert a data class to a map
+    fun <T> T.serializeToMap(): Map<String, Any> {
+        return convert()
+    }
+
+    //convert a map to a data class
+    inline fun <reified T> Map<String, Any>.toDataClass(): T {
+        return convert()
+    }
+
+    //convert an object of type I to type O
+    inline fun <I, reified O> I.convert(): O {
+        val json = gson.toJson(this)
+        return gson.fromJson(json, object : TypeToken<O>() {}.type)
+    }
 
 
 } //End DataClass
