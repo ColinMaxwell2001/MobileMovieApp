76ff0cb17d8f237103e44c19f7d10f985c215cb3
Newbtree45 <56883501+Newbtree45@users.noreply.github.com>
2022-04-04 16:47:43 -0400

    Newberts_CodeReview_2
    Added a main view model class that implements the movie service class and its fetch movies function. I also added compose dependencies to the app build gradle.

---
+++ app/src/main/java/com/example/mymovieinfo/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.mymovieinfo
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mymovieinfo.dto.Movie
+import com.example.mymovieinfo.service.MovieService
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    // Implements the movie service fetch movies list to create a live data list of the movies
+    var movies : MutableLiveData<List<Movie>> = MutableLiveData<List<Movie>>()
+    var movieService : MovieService = MovieService()
+    fun fetchCountries() {
+        viewModelScope.launch{
+            var innerMovies = movieService.fetchMovies()
+            movies.postValue(innerMovies)
+        }
+
+    }
+
+}
