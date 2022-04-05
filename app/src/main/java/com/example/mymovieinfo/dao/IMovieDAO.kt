4e32f46f2c7c49db6e4bc97d738c4d824451f360
ColinMaxwell2001 <95194573+ColinMaxwell2001@users.noreply.github.com>
2022-04-05 18:06:18 -0400

    Update IMovieDAO.kt

--- app/src/main/java/com/example/mymovieinfo/dao/IMovieDAO.kt
+++ app/src/main/java/com/example/mymovieinfo/dao/IMovieDAO.kt
@@ -3,9 +3,13 @@ package com.example.mymovieinfo.dao
 import retrofit2.Call
 import com.example.mymovieinfo.dto.Movie
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface IMovieDAO {
 
     @GET("/b/6227c3b9a703bb674925773a")
     fun getAllMovies() : Call<ArrayList<Movie>>
+
+    @GET("/b/6227c3b9a703bb674925773a")
+    fun getMovies(@Query("Combined_Name") movieName:String): Call<ArrayList<Movie>>
 }
