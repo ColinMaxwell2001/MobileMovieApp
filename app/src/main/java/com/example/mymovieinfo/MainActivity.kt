4a584255cb6d05e9fb87fbb013345e3b293eeacc
JMaz-15 <65188299+JMaz-15@users.noreply.github.com>
2022-04-05 17:27:29 -0400

    Added code in main activity to get the inputted year movie name and results from the Activity_Main.xml file

--- app/src/main/java/com/example/mymovieinfo/MainActivity.kt
+++ app/src/main/java/com/example/mymovieinfo/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.mymovieinfo
+import android.annotation.SuppressLint
 import android.app.SearchManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.io.IOException
 import java.io.InputStream
-import android.content.Context;
+import android.content.Context
 import android.content.Intent
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val year = findViewById<TextView>(R.id.txtYear)
+        val name = findViewById<TextView>(R.id.txtMovieName)
+        val result = findViewById<TextView>(R.id.txtResults)
+
+
+        val btnFindMyMovie = findViewById<Button>(R.id.btnFindMyMovie)
+        btnFindMyMovie.setOnClickListener {
+            // your code to perform when the user clicks on the button
+
+            result.text = "the $name was released in $year"
+        }
     }
+
+
 
 
 
