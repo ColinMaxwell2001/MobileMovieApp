35d90a0d6026ce68da4422d984487edc4bd875c1
schottlw <schottlw@mail.uc.edu>
2022-04-05 11:33:10 -0400

    Removed redundant visibility modifier

--- app/src/main/java/com/example/mymovieinfo/dto/Movie.kt
+++ app/src/main/java/com/example/mymovieinfo/dto/Movie.kt
@@ -5,7 +5,7 @@ import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 
-public data class Movie(@SerializedName("rank")var rank: Int, @SerializedName("title")var title: String,
+data class Movie(@SerializedName("rank")var rank: Int, @SerializedName("title")var title: String,
                  @SerializedName("country") var country: String, @SerializedName("boxOfficeGross")var boxOfficeGross: Double,
                  @SerializedName("openingWeekendGross")var openingWeekendGross: Double, @SerializedName("distributor")var distributor: String){
 
