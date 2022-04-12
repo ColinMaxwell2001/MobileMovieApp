5b4c0b6ae1a2c442cc5ec22926bc98eb8de6b5ab
YifeiYang20 <yang3yf@mail.uc.edu>
2022-04-12 00:17:01 -0400

    4.11.2

--- app/src/main/java/com/example/mymovieinfo/MainActivity.kt
+++ app/src/main/java/com/example/mymovieinfo/MainActivity.kt
@@ -1,43 +1,48 @@
 package com.example.mymovieinfo
-import android.app.SearchManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.io.IOException
-import java.io.InputStream
-import android.content.Context;
-import android.content.Intent
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView
-import android.widget.Toast;
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
 import com.example.mymovieinfo.dto.Movie
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import androidx.compose.runtime.livedata.observeAsState
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var selectedMovie : Movie? = null
+    private var inMovieName: String = ""
+    var MovieName = (findViewById<View>(R.id.MovieName) as EditText)
+        .text.toString()
+    var MovieCountry = (findViewById<View>(R.id.MovieCountry) as EditText)
+        .text.toString()
 
    private val viewModel: MainViewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-
-        var year = findViewById(R.id.txt_Year) as TextView
-        var name = findViewById(R.id.txt_MovieName) as TextView
-        var result = findViewById(R.id.txt_Results) as TextView
-
 
         val btn_FindMyMovie = findViewById(R.id.btn_FindMyMovie) as Button
         btn_FindMyMovie.setOnClickListener {
-            // your code to perform when the user clicks on the button
 
-            result.text = "the $name was released in $year"
-
-
-            //Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
         }
         setContent {
             viewModel.fetchCountries()
@@ -46,6 +51,82 @@ class MainActivity : AppCompatActivity() {
     }
 
     @Composable
-    annotation class Composable
+    fun TextFieldWithDropdownUsage(dataIn: List<Movie>, label: String = "", take: Int = 3) {
+        val dropDownOptions = remember { mutableStateOf(listOf<Movie>()) }
+        val textFieldValue = remember { mutableStateOf(TextFieldValue()) }
+        val dropDownExpanded = remember { mutableStateOf(false) }
+        fun onDropdownDismissRequest() {
+            dropDownExpanded.value = false
+        }
+        fun onValueChanged(value: TextFieldValue) {
+            inMovieName = value.text
+            dropDownExpanded.value = true
+            textFieldValue.value = value
+            dropDownOptions.value = dataIn.filter {
+                it.toString().startsWith(value.text) && it.toString() != value.text
+            }.take(take)
+        }
+        TextFieldWithDropdown(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp),
+            value = textFieldValue.value,
+            setValue = ::onValueChanged,
+            onDismissRequest = ::onDropdownDismissRequest,
+            dropDownExpanded = dropDownExpanded.value,
+            list = dropDownOptions.value,
+            label = label
+        )
+    }
+
+    @Composable
+    fun TextFieldWithDropdown(
+        modifier: Modifier = Modifier,
+        value: TextFieldValue,
+        setValue: (TextFieldValue) -> Unit,
+        onDismissRequest: () -> Unit,
+        dropDownExpanded: Boolean,
+        list: List<Movie>,
+        label: String = ""
+    ) {
+        Box(modifier) {
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusChanged { focusState ->
+                        if (!focusState.isFocused)
+                            onDismissRequest()
+                    },
+                value = value,
+                onValueChange = setValue,
+                label = { Text(label) },
+                colors = TextFieldDefaults.outlinedTextFieldColors()
+            )
+            DropdownMenu(
+                expanded = dropDownExpanded,
+                properties = PopupProperties(
+                    focusable = false,
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true
+                ),
+                onDismissRequest = onDismissRequest
+            ) {
+                list.forEach { text ->
+                    DropdownMenuItem(onClick = {
+                        setValue(
+                            TextFieldValue(
+                                text.toString(),
+                                TextRange(text.toString().length)
+                            )
+                        )
+                        selectedMovie = text
+                    }) {
+                        Text(text = text.toString())
+                    }
+                }
+            }
+        }
+    }
+
 
 }
