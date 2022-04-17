92bbef6c60cc2e71e40458bf40ade13e3fabd522
YifeiYang20 <yang3yf@mail.uc.edu>
2022-04-17 18:35:59 -0400

    4.11.2

--- app/src/main/java/com/example/mymovieinfo/MainActivity.kt
+++ app/src/main/java/com/example/mymovieinfo/MainActivity.kt
@@ -2,22 +2,6 @@ package com.example.mymovieinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.PopupProperties
-import com.example.mymovieinfo.dto.Movie
 import java.io.IOException
 import java.io.InputStream
 
@@ -27,92 +11,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    private var selectedMovie : Movie? = null
-    private var inMovieName: String = ""
-    var MovieName = (findViewById<View>(R.id.MovieName) as EditText)
-        .text.toString()
-    var MovieCountry = (findViewById<View>(R.id.MovieCountry) as EditText)
-        .text.toString()
-
-
-
-
-    @Composable
-    fun TextFieldWithDropdownUsage(dataIn: List<Movie>, label: String = "", take: Int = 3) {
-        val dropDownOptions = remember { mutableStateOf(listOf<Movie>()) }
-        val textFieldValue = remember { mutableStateOf(TextFieldValue()) }
-        val dropDownExpanded = remember { mutableStateOf(false) }
-        fun onDropdownDismissRequest() {
-            dropDownExpanded.value = false
-        }
-        fun onValueChanged(value: TextFieldValue) {
-            inMovieName = value.text
-            dropDownExpanded.value = true
-            textFieldValue.value = value
-            dropDownOptions.value = dataIn.filter {
-                it.toString().startsWith(value.text) && it.toString() != value.text
-            }.take(take)
-        }
-        TextFieldWithDropdown(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 10.dp, end = 10.dp),
-            value = textFieldValue.value,
-            setValue = ::onValueChanged,
-            onDismissRequest = ::onDropdownDismissRequest,
-            dropDownExpanded = dropDownExpanded.value,
-            list = dropDownOptions.value,
-            label = label
-        )
-    }
-
-    @Composable
-    fun TextFieldWithDropdown(
-        modifier: Modifier = Modifier,
-        value: TextFieldValue,
-        setValue: (TextFieldValue) -> Unit,
-        onDismissRequest: () -> Unit,
-        dropDownExpanded: Boolean,
-        list: List<Movie>,
-        label: String = ""
-    ) {
-        Box(modifier) {
-            TextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .onFocusChanged { focusState ->
-                        if (!focusState.isFocused)
-                            onDismissRequest()
-                    },
-                value = value,
-                onValueChange = setValue,
-                label = { Text(label) },
-                colors = TextFieldDefaults.outlinedTextFieldColors()
-            )
-            DropdownMenu(
-                expanded = dropDownExpanded,
-                properties = PopupProperties(
-                    focusable = false,
-                    dismissOnBackPress = true,
-                    dismissOnClickOutside = true
-                ),
-                onDismissRequest = onDismissRequest
-            ) {
-                list.forEach { text ->
-                    DropdownMenuItem(onClick = {
-                        setValue(
-                            TextFieldValue(
-                                text.toString(),
-                                TextRange(text.toString().length)
-                            )
-                        )
-                        selectedMovie = text
-                    }) {
-                        Text(text = text.toString())
-                    }
-                }
-            }
-        }
-    }
 
 }
