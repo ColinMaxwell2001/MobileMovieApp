8f5022d121cc5667a440abdc5fab8307513fb56f
YifeiYang20 <yang3yf@mail.uc.edu>
2022-04-18 02:55:28 -0400

    1

--- app/src/main/java/com/example/mymovieinfo/dto/Specimen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mymovieinfo.dto
-
-class Specimen(var MovieTitle: String = "", var MovieRank: Int = 0, var MovieCountry : String = "", var MovieBoxOfficeGross: String = "", var MovieOpeningWeekendGross : String = "", var MovieDistributor : String = "") {
-    override fun toString(): String {
-        return "$MovieTitle"
-    }
-}
